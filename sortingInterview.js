b1cbd226554f72b14f6c70aa9a2378ec55318634
Emirhan Kaplan <emskaplann@gmail.com>
2020-10-20 20:08:53 -0400

    sorting questions added to study.

---
+++ sortingInterview.js
@@ -0,0 +1,26 @@
+//#1 - Sort 10 schools around your house by distance: Insertion Sort
+
+//#2 - eBay sorts listings by the current Bid amount: Radix or Counting Sort
+
+//#3 - Sport scores on ESPN: Quick Sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge Sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada: Quick or Merge Sort
+
+//#7 - Large user name database needs to be sorted. Data is very random: Quick Sort
+
+//#8 - You want to teach sorting for the first time: Bubble or Selection or Insertion
+
+
+// Mainly used sorting algos: Quick, Merge and Insertion
+
+// Radix & Counting Sort are used when the data has a range and doesn't have decimal values like 1.32 which means all the data in the set should be integer.
+
+// You need to consider Worst Case(O(n^2)) when using Quick Sort, It's not like Merge in that way also, picking up the pivot point is important in Quick Sort.
+
+// Insertion is used when the data is small or pre sorted.
+
+// Merge is the best algo in most cases. O(n log n) in all cases.
